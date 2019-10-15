10d0964062680fc99edfbdc3d218d13a2d18e53d
Benjamin <DNAblue2112@gmail.com>
2019-10-15 20:07:12 +1030

    Modified to put in album folders

--- src/main/java/net/lapismc/plex/PlexMusic.java
+++ src/main/java/net/lapismc/plex/PlexMusic.java
@@ -4,7 +4,6 @@ import org.apache.commons.io.FileUtils;
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.exceptions.CannotReadException;
-import org.jaudiotagger.audio.exceptions.CannotWriteException;
 import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.tag.FieldKey;
@@ -17,18 +16,23 @@ import java.util.Objects;
 
 class PlexMusic {
 
+    private File folder = new File("./Music");
+
     PlexMusic() {
-        File folder = new File("./Music");
         for (File f : Objects.requireNonNull(folder.listFiles())) {
-            if (f.isDirectory())
+            if (f.isDirectory() || !f.getName().endsWith(".mp3"))
                 continue;
             String artist = getArtist(f);
             try {
-                FileUtils.moveFileToDirectory(f, new File(folder, artist), true);
+                FileUtils.moveFileToDirectory(f, generateDir(f), true);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private File generateDir(File f) {
+        return new File(folder, getArtist(f) + File.separator + getAlbum(f));
     }
 
     private String getArtist(File f) {
@@ -39,22 +43,26 @@ class PlexMusic {
             Tag tag = audioFile.getTag();
 
             artist = tag.getFirst(FieldKey.ARTIST);
-            String title = tag.getFirst(FieldKey.TITLE);
-            String album = tag.getFirst(FieldKey.ALBUM);
-
-            tag = audioFile.createDefaultTag();
-            tag.setField(FieldKey.ALBUM_ARTIST, artist);
-            tag.setField(FieldKey.ARTIST, artist);
-            tag.setField(FieldKey.TITLE, title);
-            tag.setField(FieldKey.ALBUM, album);
-
-            audioFile.setTag(tag);
-            audioFile.commit();
-        } catch (CannotReadException | IOException | TagException | ReadOnlyFileException | InvalidAudioFrameException | CannotWriteException e) {
+        } catch (CannotReadException | IOException | TagException | ReadOnlyFileException | InvalidAudioFrameException e) {
             e.printStackTrace();
         }
         artist = artist.replaceAll("[\\/:*?\"<>|]", "");
         return !artist.equals("") ? artist : "Unknown Artist";
+    }
+
+    private String getAlbum(File f) {
+        AudioFile audioFile;
+        String album = "";
+        try {
+            audioFile = AudioFileIO.read(f);
+            Tag tag = audioFile.getTag();
+
+            album = tag.getFirst(FieldKey.ALBUM);
+        } catch (CannotReadException | IOException | TagException | ReadOnlyFileException | InvalidAudioFrameException e) {
+            e.printStackTrace();
+        }
+        album = album.replaceAll("[\\/:*?\"<>|]", "");
+        return !album.equals("") ? album : "Unknown Album";
     }
 
 }
